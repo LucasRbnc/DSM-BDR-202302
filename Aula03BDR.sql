6c15f1d4d3115eb5ae5ec220ebaacc4d440287a3
LucasRbnc <125413068+LucasRbnc@users.noreply.github.com>
2023-08-15 22:57:52 -0300

    Update Aula03BDR.sql

--- Aula03BDR.sql
+++ Aula03BDR.sql
@@ -1,2 +1,16 @@
 drop database if exists bd_aula03;
 create database bd_aula03;
+
+create table tbl_cliente(
+  codigo_cliente integer PRIMARY KEY ,
+  nome VARCHAR(50) NOT NULL,
+  cidade VARCHAR(50),
+  endereco VARCHAR(50)
+  );
+create table tbl_titulo(
+  codigo_titulo integer PRIMARY KEY,
+  titulo VARCHAR(50) NOT NULL,
+  descricao VARCHAR(50),
+  categoria VARCHAR(50) IN (DRAMA, COMEDIA)
+);
+  
